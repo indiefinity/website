18512deed3110c6c0c3e62b82c185cc23bf803c6
viluzi <ville@kastell.tk>
2019-04-14 13:22:04 +0300

    palautettu :))

---
+++ public/hamham/emokid.js
@@ -0,0 +1,10 @@
+console.log('harambe"');
+
+function katso(id) {
+    if (document.getElementById(id).value == 'harambe') {
+        document.getElementById('teksti').innerHTML = 'guud';
+    }
+    else {
+        document.getElementById('teksti').innerHTML = 'baad';
+    }
+}
