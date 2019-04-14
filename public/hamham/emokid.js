cfee9d5f8af43d935e6aaebdbb6328b1d776af2b
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-04-14 15:27:59 +0300

    1

--- public/hamham/emokid.js
+++ public/hamham/emokid.js
@@ -1,5 +1,8 @@
 function check(id) {
     if (document.getElementById(id).value == 'egg') {
-        document.getElementById('pepe').innerHTML = 'Wah?';
+        document.getElementById('pepe').innerHTML = 'EEEEEGGGGGGGGG';
+    }
+    else {
+        document.getElementById('pepe').innerHTML = '';
     }
 }
