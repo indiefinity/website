217e5527e4def0ac9b19551b6b2925f16b027f8c
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-04-14 13:25:57 +0300

    121213

--- public/hamham/emokid.js
+++ public/hamham/emokid.js
@@ -1,10 +1,21 @@
-console.log('harambe"');
-
-function katso(id) {
-    if (document.getElementById(id).value == 'harambe') {
-        document.getElementById('teksti').innerHTML = 'guud';
-    }
-    else {
-        document.getElementById('teksti').innerHTML = 'baad';
+function homoile(id) {
+    var homo = document.getElementById(id).value;
+    if (homo == "syö") {
+        document.getElementById('pepe').innerHTML = '!==!*IIII';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = '!==!o.III';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = ' !==!*III';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = ' !==!o.II';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = '  !==!*II';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = '  !==!o.I';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = '   !==!*I';
+        await sleep(500);
+        document.getElementById('pepe').innerHTML = '   !==!o.';
+        await sleep(500);
     }
 }
