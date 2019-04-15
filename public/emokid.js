3fc7233f7a7a8167f301a5aee4714164c1cdc6a2
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-04-15 19:40:21 +0300

    33

--- public/emokid.js
+++ public/emokid.js
@@ -1,3 +1,12 @@
+function muokkaPepe(miksi) {
+    document.getElementById('pepe').innerHTML() = miksi;
+}
 function fuu() {
-    
+    var inp = document.getElementById('box').value();
+    if (inp == 'kakka') {
+        muokkaPepe('Tykk‰‰n sinusta');
+    }
+    else if (inp == 'egg') {
+        muokkaPepe('EGG');
+    }
 }
