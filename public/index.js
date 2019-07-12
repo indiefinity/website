03c43922c70a39f094d046b7dd33134e94ed1786
viluzi <viluzi.game@gmail.com>
2019-07-12 23:29:32 +0300

    now starting done

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ var startingDegree = r(0,180)
 function reset() {
     startingDegree = r(0,180)
     console.log(startingDegree)
-    document.getElementById('text').style.rotate(startingDegree)
+    document.getElementById('text').style.transform = "rotate(" + startingDegree + "deg)"
 }
 function r(min, max) {
   return Math.floor(min + Math.random()*(max + 1 - min))
