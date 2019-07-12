cf0c9b5443d608a51f849fb5e5b0dc447d8a60f7
viluzi <viluzi.game@gmail.com>
2019-07-12 23:26:11 +0300

    really stupid mistake

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 var startingDegree = r(0,180)
-function reset()
+function reset() {
     startingDegree = r(0,180)
     console.log(startingDegree)
     document.getElementById('text').style.rotate(startingDegree)
+}
 function r(min, max) {
   return Math.floor(min + Math.random()*(max + 1 - min))
 }
