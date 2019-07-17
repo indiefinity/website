c29794ed6cb1f1681e7301cd47b63855226ac5a8
viluzi <viluzi.game@gmail.com>
2019-07-17 16:28:06 +0300

    hard reset + menu beginninininin summa summaarumumutus

--- public/index.js
+++ public/index.js
@@ -1,40 +1,22 @@
-var startingDegree = r(0,360)
-var startingSpeed = r(100,1000)
-var currentSpeed = startingSpeed
-var currentDegree = startingDegree
-var timeout = 0
-function reset() {
-    startingDegree = r(0,360)
-    startingSpeed = r(0,100)
-    currentSpeed = startingSpeed
-    currentDegree = startingDegree
-    console.log(startingDegree)
-    document.getElementById('text').style.transform = "rotate(" + startingDegree + "deg)"
-}
-function r(min, max) {
-  return Math.floor(min + Math.random()*(max + 1 - min))
-}
-reset()
-spin()
-function spin() {
-  setTimeout(function() {
-    spin();
-  }, 20);
-    currentDegree = currentDegree * currentSpeed
-    document.getElementById('text').style.transform = "rotate(" + currentDegree + "deg)"
-    currentSpeed = currentSpeed * 0.95
-    
-    if (currentSpeed < 1 && timeout == 0) {
-      timeout = 5
-    setTimeout(function() {
-      reset();
-      
-    }, 5000);
-    } else {
-      if (currentSpeed < 1 && !timeout == 0) {
-        setTimeout(function() {
-          timeout -= 1
-        }, 1000)
-      }
+var red = 255
+var blue = 0
+var green = 0
+function lol() {
+    if (red > 0 && blue == 0) {
+        red--;
+        green++;
     }
+    else if (green > 0) {
+        green--;
+        blue++;
+    }
+    else if (blue > 0) {
+        blue--;
+        red++;
+    }
+    document.body.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue +")";
+    setTimeout(function(){
+        lol();
+    }, 5);
 }
+lol()
