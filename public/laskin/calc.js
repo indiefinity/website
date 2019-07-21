d4cab5bf885e52970c70006cdfe1c7dd3b32ee7b
viluzi <viluzi.game@gmail.com>
2019-07-21 12:18:40 +0300

    Laskimen alku.

---
+++ public/laskin/calc.js
@@ -0,0 +1,9 @@
+function selector(sel) {
+    document.getElementById('selector').style.display = "none"
+    document.getElementById(sel).style.display = "block"
+}
+function back() {
+    document.getElementById('selector').style.display = "block"
+    document.getElementById('button').style.display = "none"
+    document.getElementById('text').style.display = "none"
+}
