26bdcff9472268f0b048cdfbe82506c674d716e2
viluzi <ville@kastell.tk>
2019-08-05 18:30:32 +0300

    Update index.js

--- public/dadjoke/index.js
+++ public/dadjoke/index.js
@@ -7,9 +7,16 @@ function read() {
         }, success: function(data){
             console.log(data);
             text.innerHTML = data.joke
+            speak(data.joke)
         }
 })
  }
 function button() {
     read()
+}
+var ut;
+function speak(what) {
+    ut = new SpeechSynthesisUtterance(what)
+    window.speechSynthesis.cancel(ut)
+    window.speechSynthesis.speak(ut)
 }
