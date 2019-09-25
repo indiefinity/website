22958c3a9d53f715b9609f5ed3514e2853593766
viluzi <47392011+viluzi@users.noreply.github.com>
2019-09-25 08:51:30 +0300

    Update index.js

--- public/canvas/index.js
+++ public/canvas/index.js
@@ -15,6 +15,7 @@ img.onload = function(){
 };
 img.src = "http://www.pngmart.com/files/6/Pigeon-PNG-Pic.png";
 img.width = 200
+img.height = 200
 console.log("start!")
 pipe()
 var char = {"x":100,"y":200}
