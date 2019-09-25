3e719521b3d88136192f087ce35beeada2b94ba6
viluzi <47392011+viluzi@users.noreply.github.com>
2019-09-25 08:56:03 +0300

    Update index.js

--- public/canvas/index.js
+++ public/canvas/index.js
@@ -13,7 +13,7 @@ img.onload = function(){
     bird = img
     //ctx.drawImage(img,0,0);
 };
-img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU0j7K5S5Cz3lwnHG6bW0Dp-Ecoetz5Ff-pivCk2W3EMkckLw_Fg";
+img.src = "http://pixelartmaker.com/art/53cc65bf7037ac5.png";
 img.width = 200
 img.height = 200
 console.log("start!")
