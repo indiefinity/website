bac634d9c65b9960a05f8f2af9a780d9fe9b7a0a
viluzi <47392011+viluzi@users.noreply.github.com>
2019-10-16 09:08:42 +0300

    Update index.js

--- public/canvas/index.js
+++ public/canvas/index.js
@@ -46,9 +46,9 @@ var gravity = setInterval(function() {
 window.addEventListener( "keypress", doKeyDown, false);
 c.addEventListener( "keydown", doKeyDown, true);
 function doKeyDown(e) {
-    if (e == 32) { jump() }
-    if (e == 68) { char.x++ }
-    if (e == 65) { char.x-- }
+    if (e.keycode == 32) { jump() }
+    if (e.keycode == 68) { char.x++ }
+    if (e.keycode == 65) { char.x-- }
 };
 //cool shit land
 function jump() {
