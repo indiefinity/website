840fee8a3e5e7d89f985f71b0ecedcd1042eb6bd
viluzi <47392011+viluzi@users.noreply.github.com>
2020-01-22 09:29:57 +0200

    Update index.js

--- public/tyham/index.js
+++ public/tyham/index.js
@@ -5,11 +5,11 @@ function next() {
     if (pepe == 1) { document.getElementById("pepe").style.backgroundColor = "white" }
     if (pepe == 2) { document.getElementById("pepe").style.backgroundColor = "yellow" }
     if (pepe == 3) { document.getElementById("pepe").style.backgroundColor = "orange" }
-    if (pepe == 4) { document.getElementById("pepe").style.backgroundColor = "gray" }
+    if (pepe == 4) { document.getElementById("pepe").style.backgroundColor = "red" }
     if (pepe == 4) { pepe = 0 }
     
 }
 var main = setInterval(function(){
     next()
     pepe += 1;
-}, 125)
+}, 75)
