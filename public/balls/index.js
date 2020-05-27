3f334dc1ef7989354d4d1b2e3836067e6352f08a
viluzi <ville@kastell.tk>
2020-05-27 08:46:39 +0300

    nice

---
+++ public/balls/index.js
@@ -0,0 +1,43 @@
+let balls = []
+function setup() {
+    createCanvas(1000, 1000);
+    angleMode(DEGREES);
+    balls[0] = new Ball();
+  }
+  
+  function draw() {
+    background(0);
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].move();
+        balls[i].draw();
+    }
+  }
+  class Ball {
+    constructor() {
+        this.radius = 15;
+        this.color = color(0,0,255);
+        this.x = 100;
+        this.y = 100;
+    }
+    draw() {
+        colorMode(HSB);
+        fill(this.color);
+        circle(this.x, this.y, 2 * this.radius);
+
+    }
+    move() {
+        if (this.x <= 900 && this.y <= 100) {
+        this.x++
+        }
+        if (this.x >= 900 && this.y <=900) {
+            this.y++
+        }
+        if (this.x >= 100 && this.y >= 900) {
+            console.log("hi")
+            this.x = this.x - 1
+        }
+        if (this.x <= 100 && this.y <=900) {
+            this.y = this.y - 1
+        }
+}
+}
